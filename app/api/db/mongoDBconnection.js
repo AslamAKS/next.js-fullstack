904e7a4f7d8146757c3ec86635bbb3a617afcc12
AslamAKS <aslamaksofficial@gmail.com>
2025-03-08 09:47:44 +0530

    sat8mar09:47

--- app/api/db/mongoDBconnection.js
+++ app/api/db/mongoDBconnection.js
@@ -1,31 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI;
 
 if (!MONGODB_URI) {
   // throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-  console.log("No DB Found")
+  console.log("No DB Found");
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+// let cached = global.mongoose || { conn: null, promise: null };
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  // if (cached.conn) {
+  //   return cached.conn;
+  // }
 
-  if (!cached.promise) {
-
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  // if (!cached.promise) {
+  //   cached.promise = mongoose.connect(MONGODB_URI);
+  // }
+  // cached.conn = await cached.promise;
+  // return cached.conn;
+  mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
 }
 
 export default dbConnect;
